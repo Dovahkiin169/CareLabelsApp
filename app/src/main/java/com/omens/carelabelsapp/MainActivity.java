92128a3646d1cf90d0a7185476faa1d3a8c3fe18
Dmytro Verkhovsky <36972220+Dovahkiin169@users.noreply.github.com>
2020-08-12 21:49:00 +0200

    Adding custom layouts for implementing Labeling

--- app/src/main/java/com/omens/carelabelsapp/MainActivity.java
+++ app/src/main/java/com/omens/carelabelsapp/MainActivity.java
@@ -1,8 +1,17 @@
 package com.omens.carelabelsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageButton Wash = (ImageButton) findViewById(R.id.iconWashing);
+        Wash.setOnClickListener(mCorkyListener);
+
+        ConstraintLayout emptyView = (ConstraintLayout) findViewById(R.id.care_in_main);
+        emptyView.setOnClickListener(mCorkyListener);
     }
+
+    private View.OnClickListener mCorkyListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            TextView e =  (TextView) findViewById(R.id.item_description);
+            switch (v.getId()) {
+                case R.id.care_in_main:
+                    e.setVisibility(View.GONE);
+                    break;
+                case R.id.iconWashing:
+                    if(e.getVisibility() == View.GONE)
+                    break;
+                case R.id.iconBleach:
+
+                    break;
+                case R.id.iconDrying:
+
+                    break;
+                case R.id.iconIroning:
+
+                    break;
+                case R.id.iconProfessionalCleaning:
+
+                    break;
+            }
+        }
+    };
 }
