76166326903f9b0e53f33fb6c9abccc419447cda
Dmytro Verkhovsky <36972220+Dovahkiin169@users.noreply.github.com>
2020-08-13 17:58:46 +0200

    Adding labels Layout

--- app/src/main/java/com/omens/carelabelsapp/MainActivity.java
+++ app/src/main/java/com/omens/carelabelsapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.omens.carelabelsapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Layout;
 import android.util.Log;
@@ -13,44 +14,78 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageButton WashIcon;
+    ImageButton BleachIcon;
+    ImageButton DryingIcon;
+    ImageButton IroningIcon;
+    ImageButton ProfessionalCleaningIcon;
+    ConstraintLayout CareLabelLayout;
+    TextView ItemDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        WashIcon = findViewById(R.id.iconWashing);
+        BleachIcon = findViewById(R.id.iconBleach);
+        DryingIcon = findViewById(R.id.iconDrying);
+        IroningIcon = findViewById(R.id.iconIroning);
+        ProfessionalCleaningIcon = findViewById(R.id.iconProfessionalCleaning);
 
-        ImageButton Wash = (ImageButton) findViewById(R.id.iconWashing);
-        Wash.setOnClickListener(mCorkyListener);
+        ItemDescription = findViewById(R.id.item_description);
 
-        ConstraintLayout emptyView = (ConstraintLayout) findViewById(R.id.care_in_main);
-        emptyView.setOnClickListener(mCorkyListener);
+        WashIcon.setOnClickListener(this);
+        BleachIcon.setOnClickListener(this);
+        DryingIcon.setOnClickListener(this);
+        IroningIcon.setOnClickListener(this);
+        ProfessionalCleaningIcon.setOnClickListener(this);
+
+        CareLabelLayout = findViewById(R.id.care_in_main);
+        CareLabelLayout.setOnClickListener(this);
+        SetClickable(false);
     }
 
-    private View.OnClickListener mCorkyListener = new View.OnClickListener() {
-        public void onClick(View v) {
-            TextView e =  (TextView) findViewById(R.id.item_description);
-            switch (v.getId()) {
-                case R.id.care_in_main:
-                    e.setVisibility(View.GONE);
-                    break;
-                case R.id.iconWashing:
-                    if(e.getVisibility() == View.GONE)
-                    break;
-                case R.id.iconBleach:
-
-                    break;
-                case R.id.iconDrying:
-
-                    break;
-                case R.id.iconIroning:
-
-                    break;
-                case R.id.iconProfessionalCleaning:
-
-                    break;
-            }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.care_in_main:
+                if(ItemDescription.getVisibility() == View.VISIBLE) {
+                    SetClickable(true);
+                    CareLabelLayout.setClickable(false);
+                    ItemDescription.setVisibility(View.GONE);
+                }
+                break;
+            case R.id.iconWashing:
+                ButtonPresser(true,false,false,false,false);
+                break;
+            case R.id.iconBleach:
+                ButtonPresser(false,true,false,false,false);
+                break;
+            case R.id.iconDrying:
+                ButtonPresser(false,false,true,false,false);
+                break;
+            case R.id.iconIroning:
+                ButtonPresser(false,false,false,true,false);
+                break;
+            case R.id.iconProfessionalCleaning:
+                ButtonPresser(false,false,false,false,true);
+                break;
         }
-    };
+    }
+    public void SetClickable(boolean data) {
+        WashIcon.setClickable(data);
+        BleachIcon.setClickable(data);
+        DryingIcon.setClickable(data);
+        IroningIcon.setClickable(data);
+        ProfessionalCleaningIcon.setClickable(data);
+    }
+    public void ButtonPresser(boolean one, boolean two, boolean three, boolean four, boolean five ) {
+        WashIcon.setSelected(one);
+        BleachIcon.setSelected(two);
+        DryingIcon.setSelected(three);
+        IroningIcon.setSelected(four);
+        ProfessionalCleaningIcon.setSelected(five);
+    }
 }
